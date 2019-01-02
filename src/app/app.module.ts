0085e9d197803bf01dc385dc0759125dca18f934
Joel Tanzi <jtanziredux@gmail.com>
2019-01-01 19:58:13 -0600

    Created repository.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 import { AppComponent } from './app.component';
+import { MovieComponent } from './movie/movie.component';
+import { HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/movie/movie.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+import { loadInternal } from '@angular/core/src/render3/util';
+
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.css']
+})
+export class MovieComponent implements OnInit {
+
+
+  private omdbApiUrl = 'http://www.omdbapi.com/?apikey=';
+  private apiKey = 'f805b4ff';
+  movie: any;
+  showError = false;
+  message = '';
+  searchForm: FormGroup;
+  loading$ = new BehaviorSubject(false);
+
+  constructor(
+    private http: HttpClient,
+    private formBuilder: FormBuilder
+  ) {
+    this.movie = null;
+    this.searchForm = this.formBuilder.group({
+      search: ['']
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  onSearchClicked(): void {
+
+    // Indicate loading status
+    this.loading$.next(true);
+    // Reset component variables
+    this.showError = false;
+    this.message = '';
+    const term = this.searchForm.get('search').value;
+
+    setTimeout(() => {
+
+      // Make the call to the API
+      this.http.get(`${this.omdbApiUrl}${this.apiKey}&t=${term}`).subscribe((success: any) => {
+
+        // If we get a failed search error
+        if (success.Error) {
+          this.movie = null;
+          this.message = 'Sorry, cannot find that one. Maybe try another title?';
+          this.showError = true;
+        } else {
+
+          // Populate the movie data for the form
+          this.movie = {
+            title: success.Title,
+            year: success.Year,
+            rating: success.rating,
+            runtime: success.Runtime,
+            genre: success.Genre,
+            director: success.Director
+          };
+        }
+
+        // Make the spinner go away
+        this.loading$.next(false);
+
+      }, (error) => { // HTTP error response condition
+        this.movie = null;
+        this.message = 'Sorry, that search did not work, server responded with error.';
+        this.showError = true;
+      });
+
+    }, 1500);
+
+
+  }
+
+}
+
+
